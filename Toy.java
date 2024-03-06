a9ab625b357e1762acf406f79a4d09d4abfb5fdd
sankulll <123102249+sankulll@users.noreply.github.com>
2024-03-06 17:24:03 +0300

    Toy.java

---
+++ Toy.java
@@ -0,0 +1,29 @@
+package TS;
+
+public class Toy {
+    private String id;
+    private String name;
+    private int frequency;
+
+    public Toy(String id, String name, int frequency) {
+        this.id = id;
+        this.name = name;
+        this.frequency = frequency;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+}
